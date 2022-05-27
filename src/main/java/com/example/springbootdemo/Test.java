ee2b1f491e2d6e3d6bab8b98c4edcf9765581c94
huihui0129 <3340759085@qq.com>
2022-05-27 19:34:56 +0800

    测试提交 5.27 19：34

--- src/main/java/com/example/springbootdemo/Test.java
+++ src/main/java/com/example/springbootdemo/Test.java
@@ -2,6 +2,7 @@ package com.example.springbootdemo;
 
 public class Test {
     public static void main(String[] args) {
+        Test test = new Test();
         System.out.println("test");
     }
 }
